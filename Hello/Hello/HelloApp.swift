6e4c476b62b67823c7e3e178f606d37289118b2a
Cyan75 <incanto075@gmail.com>
2021-06-20 20:34:54 +0900

    first swift UI pjt

---
+++ Hello/Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Cyan on 2021/06/20.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewHello.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View{
+        Text("Bonjour, Monde!")
+    }   
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View{
+        ContentView()
+    }
+}
+
+// func print(){
+//     print("Hello World!")
+// }
+// print()
