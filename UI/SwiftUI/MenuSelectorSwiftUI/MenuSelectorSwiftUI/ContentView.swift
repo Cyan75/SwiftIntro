165bc02c09483a5377ece6a42223f2cfbb50e1f4
Cyan75 <incanto075@gmail.com>
2022-05-26 23:57:48 +0900

    Navigation view& Picker, form group,

--- UI/SwiftUI/MenuSelectorSwiftUI/MenuSelectorSwiftUI/ContentView.swift
+++ UI/SwiftUI/MenuSelectorSwiftUI/MenuSelectorSwiftUI/ContentView.swift
@@ -8,25 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    let zones_kr = ["강남⋅서초⋅양재","분당","수지", "아주대","신촌⋅이대⋅연희","서교⋅합정⋅상수","연남⋅성산","한남","종로","마포","신림⋅서울대입구", "수유⋅우이"];
+    let zones = ["sector1","sector2","sector3","sector4","sector5","sector6","sector7"]
+    @State var zoneSelection = "";
     var body: some View {
-//        VStack{
-//            Form{
-//                Group{
-//                    Section(header: Text("Zones")){
-//                        Picker(selection: .constant(1), label: Text("Picker")) {
-//                            Text("1").tag(1)
-//                            Text("2").tag(2)
-//                            Text("3").tag(3)
-//                            Text("4").tag(4)
-//                        }
-//                    }
-//                }
-//                Text("Hullo World").font(.system(size: 25))
-//            }
-//        }
         VStack {
-            Text("Press the button below")
-                .font(.system(size: 25));
+            Text("Bienvenue à RMS")
+                .font(.system(size: 25)).multilineTextAlignment(.center);
+            Text("Random Menu Selector ")
+                .font(.system(size: 10)).multilineTextAlignment(.center);
             
             Button(action: {
                 print("Button is pressed")
@@ -35,17 +25,21 @@ struct ContentView: View {
                     .padding()
                     .font(.headline.bold())
                     .foregroundColor(.white)
-//                    .background(Color.blue.opacity(0.75));
-                    .background(Color(red: 0, green: 168, blue: 255 ,opacity: 0.75));
+                    .background(Color(red: 0.4627, green: 0.8392, blue: 1.0));
             }/*.frame(maxWidth : .infinity)*/
-            Form{
-                Group{
-                    Text("Hullo World").font(.system(size: 25));
-                    Section(header: Text("Zones")){
-                        Text("Hullo World").font(.system(size: 25));
+            NavigationView{
+                Form{
+                    Group{
+                        Section(header: Text("Zones")){
+                            Picker("Select", selection: $zoneSelection){
+                                ForEach(zones, id: \.self){ option in
+                                    Text(option)
+                                }
+                            }
+                            Text("Zone") + Text("\t\t\(zoneSelection)").;
+                        }
                     }
                 }
-
             }
         }
     }
