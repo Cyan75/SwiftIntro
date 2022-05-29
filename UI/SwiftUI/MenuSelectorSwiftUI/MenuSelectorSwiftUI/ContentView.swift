84a0645b81392e1e1e30ea46f84e791eca7123cb
Cyan75 <incanto075@gmail.com>
2022-05-29 20:11:09 +0900

    Navigation View, alignment, navigation title, form text alignment

--- UI/SwiftUI/MenuSelectorSwiftUI/MenuSelectorSwiftUI/ContentView.swift
+++ UI/SwiftUI/MenuSelectorSwiftUI/MenuSelectorSwiftUI/ContentView.swift
@@ -8,46 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
-    let zones_kr = ["강남⋅서초⋅양재","분당","수지", "아주대","신촌⋅이대⋅연희","서교⋅합정⋅상수","연남⋅성산","한남","종로","마포","신림⋅서울대입구", "수유⋅우이"];
-    let zones = ["sector1","sector2","sector3","sector4","sector5","sector6","sector7"]
-    @State var zoneSelection = "";
+    let zones_kr = ["강남⋅서초⋅양재","분당","수지", "아주대","신촌⋅이대⋅연희","서교⋅합정⋅상수","연남⋅성산","한남⋅이태원","종로","마포","신림⋅서울대입구", "수유⋅우이"];
+//    let zones = ["sector1","sector2","sector3","sector4","sector5","sector6","sector7"]
+    @State private var zoneSelection = "";
+    @State private var header = "¡Bienvenue à RMS!";
+    @State private var headerDescription = "Random Menu Selector";
+    
     var body: some View {
-        VStack {
-            Text("Bienvenue à RMS")
-                .font(.system(size: 25)).multilineTextAlignment(.center);
-            Text("Random Menu Selector ")
-                .font(.system(size: 10)).multilineTextAlignment(.center);
-            
-            Button(action: {
-                print("Button is pressed")
-            }) {
-                Text("Press me")
-                    .padding()
-                    .font(.headline.bold())
-                    .foregroundColor(.white)
-                    .background(Color(red: 0.4627, green: 0.8392, blue: 1.0));
-            }/*.frame(maxWidth : .infinity)*/
-            NavigationView{
-                Form{
-                    Group{
-                        Section(header: Text("Zones")){
-                            Picker("Select", selection: $zoneSelection){
-                                ForEach(zones, id: \.self){ option in
-                                    Text(option)
-                                }
+        VStack(spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/) {
+            NavigationView {
+                Form {
+                    Section(header: Text("Zones")) {
+                        Picker("Select", selection: $zoneSelection) {
+                            ForEach(zones_kr, id: \.self){ option in
+                                Text(option)
                             }
-                            Text("Zone") + Text("\t\t\(zoneSelection)").;
+                        }
+                        HStack(alignment: .lastTextBaseline) {
+                            Text("Zone")
+                            Text("\(zoneSelection)").frame(maxWidth: .infinity, alignment: .trailing)
+                        }
+                    }
+                }.navigationBarTitleDisplayMode(.inline).toolbar {
+                    ToolbarItem(placement: .principal) {
+                        VStack {
+                            Text(header)
+                                .font(.system(size: 25)).multilineTextAlignment(.center);
+                            Text(headerDescription).font(.system(size: 10)).multilineTextAlignment(.trailing);
                         }
                     }
                 }
-            }
         }
+            Divider()
+        BuildGetButton();
+    }
+
+        
+//    func TitleView() -> some View {
+//        let titleView = HStack(alignment: .top, spacing: /*@START_MENU_TOKEN@*/nil/*@END_MENU_TOKEN@*/) {
+//            VStack(alignment: .center) {
+//                Text(header)
+//                    .font(.system(size: 25)).multilineTextAlignment(.center);
+//                Text(headerDescription)
+//                    .font(.system(size: 10)).multilineTextAlignment(.center);
+//            }.multilineTextAlignment(.center)
+//        }
+//        return titleView;
+//    }
+    }
+    func BuildGetButton() -> some View {
+        let getBT = Button(action: {
+            // TODO : C++ RNG to return a result
+            // emulate the result on GUI
+            print("Button Pressed")
+        }){
+            Text("Give me one")
+                .padding()
+                .font(.headline.bold())
+                .foregroundColor(.white)
+                .background(Color(red: 5/225, green: 168/225, blue: 228/225));
+        }
+        return getBT;
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            
     }
 }
